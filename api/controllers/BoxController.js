27c75877a5c766ae9fffcf645232f1da42bb045e
Will Davies <wdavies973@gmail.com>
2017-11-11 00:58:26 +0000

    bug fixes

--- api/controllers/BoxController.js
+++ api/controllers/BoxController.js
@@ -43,8 +43,11 @@ module.exports = {
     
     var toReturnItems = [];
     
+
+    
     // calculate our rank
-    await(Players.find({}).exec(function(err, items) { // returns all received checkouts assosicated with this team
+    var query2 = {id: 'asdf'};
+    await(Players.find(query2).exec(function(err, items) { // returns all received checkouts assosicated with this team
         
         // we've received all the team's checkouts, let's get rid of all the ones that don't match their last edit id
         for(i = 0; i < items.length; i++) {
