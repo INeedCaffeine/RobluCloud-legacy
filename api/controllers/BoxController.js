c212163009add10a40ff91842686895e3a57ad2f
Will Davies <wdavies973@gmail.com>
2017-11-11 00:49:54 +0000

    bug fixes

--- api/controllers/BoxController.js
+++ api/controllers/BoxController.js
@@ -11,30 +11,40 @@ module.exports = {
     // Calculate rank
     var rank = 1;
     
-    // see if the player already exists
     try {
+        // Check if the player already exists
         var query = {id: req.param('id')};
         var player_ref = await(Players.findOne(query));
-        // update the value
-        try {
-            await(Players.update(query, {score: req.param('score')}));
-        } catch(err) {
-            return RespService.e(res, 'Failed to update player value');
+        
+        // Player doesn't exit
+        if(player_ref == null) {
+            try {
+                var new_player = {id: req.param('id'), score: req.param('score')};
+                await(Players.create(new_player));
+            } catch(err) {
+                return RespService.e(res, 'Failed to create user '+err);
+            }
+        
+        } else {
+        // update the value if player already exists
+            try {
+                await(Players.update(query, {score: req.param('score')}));
+            } catch(err) {
+                // It doesn't exist, create new user
+                return RespService.e(res, 'Failed to update player value');
+            }
         }
+        
     } catch(err) {
         // user didn't exist, create new user
-        try {
-            var new_player = {id: req.param('id'), score: req.param('score')};
-            await(Players.create(new_player));
-        } catch(err) {
-            return RespService.e(res, 'Failed to create user '+err);
-        }
+        return RespService.e(res, 'Error');
+        
     }
     
     var toReturnItems = [];
     
     // calculate our rank
-    await(Players.find().exec(function(err, items) { // returns all received checkouts assosicated with this team
+    await(Players.find({}).exec(function(err, items) { // returns all received checkouts assosicated with this team
         
         // we've received all the team's checkouts, let's get rid of all the ones that don't match their last edit id
         for(var i = 0; i < items.length; i++) {
