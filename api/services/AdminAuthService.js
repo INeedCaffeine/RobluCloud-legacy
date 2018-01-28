29edbd2aaeb822ba6baac3bc4ab04568ea970339
WillDavies <wdavies973@gmail.com>
2018-01-27 18:42:59 -0600

    Fixed admin aut

--- api/services/AdminAuthService.js
+++ api/services/AdminAuthService.js
@@ -6,20 +6,14 @@ module.exports = {
   /*
    * This authentication method 
    */
-  authenticate_async: function (req, f_admin_required) {
+  authenticate_async: function (req) {
     if (!req.param('auth')) throw new Error('Missing token');
     
     // lookup token and connected permissions and user info 
-    try { var user = await(Users.findOne({auth: req.param('auth')})); } 
+    try { var user = await(Admin.findOne({auth: req.param('auth')})); } 
     catch(err) { throw new Error('Token lookup problem. Check input data. ' + err); }
     
     if (!user) throw new Error('Token not found in database');
-
-    // check if admin is required
-    if (f_admin_required) {
-      if (!user.admin) throw new Error('Only admins can use this function');
-      return -1;  // if authorized, allow the requesting action to proceed
-    }
 
     return user;  // if authorized, allow the requesting action to proceed
   },
