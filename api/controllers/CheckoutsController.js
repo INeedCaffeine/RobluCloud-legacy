b718e531bd7aabf663a6c1f31d8016661303c3b8
WillDavies <wdavies973@gmail.com>
2018-02-03 12:05:04 -0600

    Updated checkouts complete status

--- api/controllers/CheckoutsController.js
+++ api/controllers/CheckoutsController.js
@@ -148,7 +148,7 @@ module.exports = {
 
         for (i = 0; i < items.length; i++) {
           // Only receive the checkout if it's completed and verified with the submitted time stamp
-          if (items[i].time > (req.param('time') / 1000) && items[i].status == 3) toReturnItems.push(items[i]);
+          if (items[i].time > (req.param('time') / 1000) && items[i].status == 2) toReturnItems.push(items[i]);
         }
       }));
 
