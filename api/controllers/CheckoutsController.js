4a876817fd1ec3f858d12881a5f279f9e16ed261
WillDavies <wdavies973@gmail.com>
2018-02-25 17:31:11 -0600

    Removed an unused logging statement

--- api/controllers/CheckoutsController.js
+++ api/controllers/CheckoutsController.js
@@ -148,8 +148,6 @@ module.exports = {
       await(Checkouts.find(query).exec(function (err, items) { // returns all received checkouts assosicated with this team
         for (i = 0; i < items.length; i++) {
           // Only receive the checkout if it's completed and verified with the submitted time stamp
-          if(items[i].status == 2) console.log('Is ' + items[i].time.getTime() + ' > ' + time + ' = ' + (items[i].time.getTime() > time));
-
           if ((items[i].status == 2) && (items[i].time.getTime() > time)) {
             toReturnItems.push(items[i]);
           }
